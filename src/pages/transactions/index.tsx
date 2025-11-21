33f8736035692105d7e663d33d161938f366b0d5
Sabrina Teodoro <sabrinateodoro1019@hotmail.com>
2025-11-21 00:28:21 -0300

    Create Search component

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,15 +1,36 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions(){
     return (<div>
         <Header/>
         <Summary/>
-        <table>
-            <tr>
-                <td width="50%">Desenvolvimento de site</td>
-                <td>R$ 12.484,00</td>
-            </tr>
-        </table>
+        <TransactionsContainer>
+            <SearchForm/>
+            <TransactionsTable>
+                <tr>
+                    <td width="50%">Desenvolvimento de site</td>
+                    <td>
+                        <PriceHighlight variant="income">
+                            R$ 12.484,00
+                        </PriceHighlight>
+                    </td>
+                    <td>Venda</td>
+                    <td>13/04/2025</td>
+                </tr>
+                <tr>
+                    <td width="50%">Yakuza 7</td>
+                    <td>
+                        <PriceHighlight variant="outcome">
+                            - R$ 7.879,00
+                        </PriceHighlight>
+                    </td>
+                    <td>Diverção</td>
+                    <td>15/04/2025</td>
+                </tr>
+            </TransactionsTable> 
+        </TransactionsContainer>
     </div>)
 }
