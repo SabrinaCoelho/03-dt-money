39a053c7ad4fc4fdf33207d7cce3433e397ed3ae
Sabrina Teodoro <sabrinateodoro1019@hotmail.com>
2025-11-28 00:17:05 -0300

    Apply useCallback and useMemo

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, type ReactNode } from "react";
+import { useCallback, useEffect, useState, type ReactNode } from "react";
 import { api } from "../lib/axios";
 import { createContext } from "use-context-selector";
 
@@ -41,33 +41,38 @@ export const TransactionsContext = createContext({} as TransactionContextType);
 export function TransactionProvider({children}: TransactionsProviderProps){
     
     const [transactions, setTransactions] = useState<Transaction[]>([])
-    async function fetchTransactions(query?: string){
-        const res = await api.get("transactions", {
-            params: {
-                _sort: "createdAt",
-                _order: "desc",
-                q: query,
-            }
-        })
-        setTransactions(res.data)
-    }
-
-    async function createTransaction(data: CreateTransactionInput){
-        const {description, price, category, type} = data;
-        const res = await api.post("transactions", {
-            description, 
-            price, 
-            category, 
-            type,
-            createdAt: new Date()
-        })
-        setTransactions(prev => [res.data, ...prev])
-    }
+    
+    const fetchTransactions = useCallback(
+        async (query?: string) => {
+            const res = await api.get("transactions", {
+                params: {
+                    _sort: "createdAt",
+                    _order: "desc",
+                    q: query,
+                }
+            })
+            setTransactions(res.data)
+        }, []
+    );
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const createTransaction = () => useCallback(
+        async (data: CreateTransactionInput) => {
+            const {description, price, category, type} = data;
+            const res = await api.post("transactions", {
+                description, 
+                price, 
+                category, 
+                type,
+                createdAt: new Date()
+            })
+            setTransactions(prev => [res.data, ...prev])
+        },[]
+    );
 
     useEffect(() => {
        // eslint-disable-next-line react-hooks/set-state-in-effect
        fetchTransactions()
-    }, [])
+    }, [fetchTransactions])
     
     return(
         <TransactionsContext.Provider value={{
